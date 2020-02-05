34d3bad6d5e5506cac77fd5009fa8016e542fbe6
bdhafin <belajarcodingdhafin@gmail.com>
2020-02-05 22:54:51 +0700

    Create profile feed item serializer

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -30,3 +30,12 @@ class UserProfileSerializer(serializers.ModelSerializer):
         )
 
         return user
+
+
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    """Serializer profile feed items"""
+
+    class Meta:
+        model = models.ProfileFeedItem
+        fields = ('id', 'user_profile', 'status_text', 'created_at')
+        extra_kwargs = {'user_profile': {'read_only': True}}
